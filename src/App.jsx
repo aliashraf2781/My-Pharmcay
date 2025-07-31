a736a685c35cb881a1d79c9b3656fa83d7f1a2ec
Ali Ashraf <aaaaaa30101975@gmail.com>
2025-07-31 18:12:04 +0300

    update pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import '@fontsource-variable/cairo';
 export default function App() {
 
   return (
-    <div>
+    <div dir='rtl'>
       <Toaster
         position="top-center"
         reverseOrder={false}
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,11 @@ import Login from '../pages/Login'
 import Register from '../pages/Register'
 import PendingPage from '../pages/PendingPage'
 import ContactUs from './../pages/ContactUs';
+import StockLayout from '../Layout/StockLayout'
+import StockUserProfile from '../pages/StockDashboard/StockUserProfile'
+import Medicines from './../pages/StockDashboard/Medicines';
+import NotFound from '../pages/NotFound'
+import Orders from './../pages/StockDashboard/Orders';
 
 
 const routes = createBrowserRouter([
@@ -37,7 +42,33 @@ const routes = createBrowserRouter([
         path: 'pending',
         element: <PendingPage />
       },
+      {
+        path: 'StockDashboard',
+        element: <StockLayout />,
+        children: [
+          {
+            index: true,
+            element: <Navigate to="profile" replace />
+          },
+          {
+            path: "profile",
+            element: <StockUserProfile />,
+          },
+          {
+            path: "Medicines",
+            element: <Medicines />,
+          },
+          {
+            path: "Orders",
+            element: <Orders />,
+          },
+        ]
+      },
+      {
+        path: '*',
+        element: <NotFound/>
+      },
     ]
-  }
+  },
 ])
 export default routes
